4fbab16573fde46cfa6f8c108c70b147efb30688
martineez <martineez@gmail.com>
2017-06-14 13:51:59 +0200

    geckodriver updated from 0.16.1 to 0.17.0 (#24)

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ var Promise = require('bluebird');
 var platform = os.platform();
 var arch = os.arch();
 
-var DOWNLOAD_MAC = 'https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-macos.tar.gz';
-var DOWNLOAD_LINUX64 = 'https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux64.tar.gz';
-var DOWNLOAD_LINUX32 = 'https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux32.tar.gz';
-var DOWNLOAD_WIN32 = 'https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-win32.zip';
-var DOWNLOAD_WIN64 = 'https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-win64.zip';
+var DOWNLOAD_MAC = 'https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-macos.tar.gz';
+var DOWNLOAD_LINUX64 = 'https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-linux64.tar.gz';
+var DOWNLOAD_LINUX32 = 'https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-linux32.tar.gz';
+var DOWNLOAD_WIN32 = 'https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-win32.zip';
+var DOWNLOAD_WIN64 = 'https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-win64.zip';
 
 // TODO: move this to package.json or something
 var downloadUrl = DOWNLOAD_MAC;
@@ -23,8 +23,7 @@ var outFile = 'geckodriver.tar.gz';
 var executable = 'geckodriver';
 
 if (platform === 'linux') {
-  // No 32-bits of geckodriver for now
-  downloadUrl = arch === 'x64' ? DOWNLOAD_LINUX64 : DOWNLOAD_LINUX64;
+  downloadUrl = arch === 'x64' ? DOWNLOAD_LINUX64 : DOWNLOAD_LINUX32;
 }
 
 if (platform === 'win32') {
--- test/index.js
+++ test/index.js
@@ -14,5 +14,5 @@ test.cb('properly extracts', t => {
 
 test('programmatic usage', t => {
   var driver = require('../lib/geckodriver')
-  t.is(driver.version, '0.16.1')
+  t.is(driver.version, '0.17.0')
 });
