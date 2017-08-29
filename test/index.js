0ddfe06a749ce04f5334f6b7a0e3f971b636311d
Myk Melez <myk@mykzilla.org>
2017-08-29 10:45:09 -0700

    call platform() on correct core module (os)

--- test/index.js
+++ test/index.js
@@ -1,12 +1,13 @@
 import test from 'ava';
 import child_process from 'child_process';
+import os from 'os';
 
 test.cb('properly extracts', t => {
   child_process.exec('node ../index.js', (error, stdout, stderr) => {
     if (error) {
       return t.fail(`exec error: ${error}`)
     }
-    if (process.platform() === 'win32') {
+    if (os.platform() === 'win32') {
       t.is(stdout, 'Downloading geckodriver... Extracting... Copying... Complete.\n');
     }
     else {
