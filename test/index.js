45e7c058b91e5a0521bccf9c75fc5ab8cbed2268
John Wynn <john.wynn.qco3@statefarm.com>
2017-10-03 14:20:12 -0500

    added regex to test

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ test.cb('properly extracts', t => {
     if (error) {
       return t.fail(`exec error: ${error}`)
     }
-    t.is(stdout, 'Downloading geckodriver from https://github.com/mozilla/geckodriver/releases/download/v0.19.0/geckodriver-v0.19.0-linux64.tar.gz...\nExtracting geckodriver.tar.gz to /home/travis/build/vladikoff/node-geckodriver...\nComplete.\n');
+    t.regex(stdout, /Downloading geckodriver from (.*)Extracting geckodriver.tar.gz to (*)...\nComplete.\n/);
     t.is(stderr, '');
     t.end();
   });
