cf328d3f1761957a40817128016911158a693ec3
Vlad Filippov <vlad.filippov@gmail.com>
2018-03-16 11:28:08 -0400

    Prepare 1.11.0

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ var baseCDNURL = process.env.GECKODRIVER_CDNURL || process.env.npm_config_geckod
 // Remove trailing slash if included
 baseCDNURL = baseCDNURL.replace(/\/+$/, '');
 
-var DOWNLOAD_MAC = baseCDNURL + '/v0.19.1/geckodriver-v0.19.1-macos.tar.gz';
-var DOWNLOAD_LINUX64 = baseCDNURL + '/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz';
-var DOWNLOAD_LINUX32 = baseCDNURL + '/v0.19.1/geckodriver-v0.19.1-linux32.tar.gz';
-var DOWNLOAD_WIN32 = baseCDNURL + '/v0.19.1/geckodriver-v0.19.1-win32.zip';
-var DOWNLOAD_WIN64 = baseCDNURL + '/v0.19.1/geckodriver-v0.19.1-win64.zip';
+var DOWNLOAD_MAC = baseCDNURL + '/v0.20.0/geckodriver-v0.20.0-macos.tar.gz';
+var DOWNLOAD_LINUX64 = baseCDNURL + '/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz';
+var DOWNLOAD_LINUX32 = baseCDNURL + '/v0.20.0/geckodriver-v0.20.0-linux32.tar.gz';
+var DOWNLOAD_WIN32 = baseCDNURL + '/v0.20.0/geckodriver-v0.20.0-win32.zip';
+var DOWNLOAD_WIN64 = baseCDNURL + '/v0.20.0/geckodriver-v0.20.0-win64.zip';
 
 // TODO: move this to package.json or something
 var downloadUrl = DOWNLOAD_MAC;
