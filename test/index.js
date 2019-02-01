88fb1b7c9a3dfd77090fa5af3258bce5eeb47100
Dave Haeffner <dhaeffner@gmail.com>
2019-02-01 21:48:27 +0200

    Bumped geckodriver version number to latest (#56)

--- test/index.js
+++ test/index.js
@@ -14,5 +14,5 @@ test.cb('properly extracts', t => {
 
 test('programmatic usage', t => {
   var driver = require('../lib/geckodriver')
-  t.is(driver.version, '0.23.0')
+  t.is(driver.version, '0.24.0')
 });
