e4ad2a9bcabd5281dd25d1753931750b4e2e8541
Christian Bromann <git@bromann.dev>
2023-07-25 00:45:31 -0700

    don't propagate cacheDir to driver

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,18 @@ import { DEFAULT_HOSTNAME, log } from './constants.js'
 import type { GeckodriverParameters } from './types.js'
 
 export async function start (params: GeckodriverParameters) {
+  const { cacheDir, ...startArgs } = params
   let geckoDriverPath = process.env.GECKODRIVER_FILEPATH || params.customGeckoDriverPath
   if (!geckoDriverPath) {
-    geckoDriverPath = await downloadDriver(params.geckoDriverVersion, params.cacheDir)
+    geckoDriverPath = await downloadDriver(params.geckoDriverVersion, cacheDir)
   }
 
   if (!(await hasAccess(geckoDriverPath))) {
     throw new Error('Failed to access Geckodriver, was it installed successfully?')
   }
 
-  params.host = params.host || DEFAULT_HOSTNAME
-
-  const args = parseParams(params)
+  startArgs.host = startArgs.host || DEFAULT_HOSTNAME
+  const args = parseParams(startArgs)
   log.info(`Starting Geckodriver at ${geckoDriverPath} with params: ${args.join(' ')}`)
   return cp.spawn(geckoDriverPath, args)
 }
