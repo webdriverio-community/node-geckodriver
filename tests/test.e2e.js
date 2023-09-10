7a5bbc9d088f5c91d431be4d1f1e648dc5c74b7e
Marco Gonzalez <marcogrcr@gmail.com>
2023-09-10 19:59:29 +0000

    Fix bug when specifying customGeckoDriverPath
    WHAT?
    
    Destructured `customGeckoDriverPath` out of `params` in
    `function start()`.
    
    Additionally, updated `test.e2e.js` to test the three `webdriver` usage
    modes: 1/ automatically download and start, 2/ automatically start, 3/
    manually start.
    
    Finally, added a self-referencing `devDependency` to `geckodriver`
    since `webdriverio` transitively depends on it.
    
    WHY?
    
    Before this change, running the following code:
    
    ```ts
    import { start } from 'geckodriver'
    
    await start({ customGeckoDriverPath: '/some/path/geckodriver' })
    ```
    
    Would fail to start the `geckodriver` because it would execute:
    
    ```shell
    geckodriver --custom-gecko-driver-path=/some/path/geckodriver --port=43083 --host=0.0.0.0 --websocket-port=0
    ```
    
    Which results in exit code `64` and the following output:
    
    ```log
    /some/path/geckodriver: error: Found argument '--custom-gecko-driver-path' which wasn't expected, or isn't valid in this context
    
    If you tried to supply `--custom-gecko-driver-path` as a value rather than a flag, use `-- --custom-gecko-driver-path`
    
    USAGE:
        geckodriver [OPTIONS]
    
    For more information try --help
    ```
    
    By destructuring `customGeckoDriverPath`, the extraneous
    `--custom-gecko-driver-path` argument is no longer specified and the
    webdriver starts successfully.
    
    HOW?
    
    Successfully ran:
    
    ```shell
    npm run build && npm test
    ```

--- tests/test.e2e.js
+++ tests/test.e2e.js
@@ -1,16 +1,63 @@
 import waitPort from 'wait-port'
 import { remote } from 'webdriverio'
 
-import { start } from '../dist/index.js'
+import { download, start } from '../dist/index.js'
+
+// start geckodriver automatically
+console.log('= start geckodriver automatically =')
+try {
+  const browser = await remote({
+    automationProtocol: 'webdriver',
+    capabilities: {
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+        args: ['-headless']
+      }
+    }
+  })
+  await browser.url('https://webdriver.io')
+  await browser.deleteSession()
+} catch (err) {
+  console.error(err)
+  process.exit(1)
+}
+
+// start specific geckodriver
+console.log('= start specific geckodriver =')
+
+const binary = await download()
+
+try {
+  const browser = await remote({
+    automationProtocol: 'webdriver',
+    capabilities: {
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+        args: ['-headless']
+      },
+      'wdio:geckodriverOptions': {
+        binary
+      }
+    }
+  })
+  await browser.url('https://webdriver.io')
+  await browser.deleteSession()
+} catch (err) {
+  console.error(err)
+  process.exit(1)
+}
+
+// start geckodriver manually
+console.log('= start geckodriver manually =')
 
 const port = 4444
 const cp = await start({ port })
 
 try {
-  await waitPort({ port: 4444 })
+  await waitPort({ port })
   const browser = await remote({
     automationProtocol: 'webdriver',
-    hostname: '0.0.0.0',
+    port, // must set port or wdio will automatically start geckodriver
     capabilities: {
       browserName: 'firefox',
       'moz:firefoxOptions': {
