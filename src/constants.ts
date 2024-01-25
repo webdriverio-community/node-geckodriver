57a1aca337f0767d0e39f02aa7ed222fc1e9db9c
Christian Bromann <git@bromann.dev>
2024-01-25 10:09:28 -0800

    (fix): pull latest release version from new endpoint (#346)

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import os from 'node:os'
 
-export const MOZ_CENTRAL_CARGO_TOML = 'https://hg.mozilla.org/mozilla-central/raw-file/tip/testing/geckodriver/Cargo.toml'
+export const GECKODRIVER_RELEASES = 'https://api.github.com/repos/mozilla/geckodriver/releases/latest'
 export const BASE_CDN_URL = process.env.GECKODRIVER_CDNURL || process.env.npm_config_geckodriver_cdnurl || 'https://github.com/mozilla/geckodriver/releases/download'
 // e.g. https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-macos-aarch64.tar.gz
 export const GECKODRIVER_DOWNLOAD_PATH = `${BASE_CDN_URL}/v%s/geckodriver-v%s-%s%s%s`
