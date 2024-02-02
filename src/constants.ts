08a5ecf24df95e9a586b18032de028576d6284aa
Christian Bromann <git@bromann.dev>
2024-02-01 22:37:44 -0800

    (fix): download raw cargo.yaml to avoid rate limiting

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import os from 'node:os'
 
-export const GECKODRIVER_RELEASES = 'https://api.github.com/repos/mozilla/geckodriver/releases/latest'
+export const GECKODRIVER_CARGO_YAML = 'https://raw.githubusercontent.com/mozilla/geckodriver/release/Cargo.toml'
 export const BASE_CDN_URL = process.env.GECKODRIVER_CDNURL || process.env.npm_config_geckodriver_cdnurl || 'https://github.com/mozilla/geckodriver/releases/download'
 // e.g. https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-macos-aarch64.tar.gz
 export const GECKODRIVER_DOWNLOAD_PATH = `${BASE_CDN_URL}/v%s/geckodriver-v%s-%s%s%s`
